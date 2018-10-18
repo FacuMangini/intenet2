d4a55c2718bbf14edd57b09087055fe2640dc041
FacuMangini <38086708+FacuMangini@users.noreply.github.com>
2018-10-18 12:09:14 -0300

    Add files via upload

--- ejercicio 2-2/script.js
+++ ejercicio 2-2/script.js
@@ -1,104 +1,112 @@
-$("#sumbit").on("click",function(event){
+$("#sumbit").on("click", function (event) {
     event.preventDefault();
     var title = $("#title").val();
     var description = $("#description").val();
     var cast = $("#cast").val();
     var gender = $("#gender").val();
     var poster = $("#poster").val();
-    var score = $("#score").val();    
+    var score = $("#score").val();
     sendData({
         title: title,
         description: description,
         cast: cast,
         poster: poster,
-        score: score,  
-        gender: gender,  
-    }).then(success,onError)
-        function success(response){
-            $(".msg").html("<p>" + response.msg + "</p>")
-                    $(".msg").show()  ;              
-        }
-        function onError(errorr){
+        score: score,
+        gender: gender,
+    }).then(success, onError)
+    function success(response) {
+        $(".msg").html("<p>" + response.msg + "</p>")
+        $(".msg").show();
+    }
+    function onError(errorr) {
 
-        };
-    
+    };
+
 })
-function sendData(params){
+function sendData(params) {
     console.log(params)
     return $.ajax({
-        method:"POST",
-        url:"http://192.168.1.170:3000/peliculas",
-        data:params
+        method: "POST",
+        url: "http://192.168.1.170:3000/peliculas",
+        data: params
     })
 }
 
-function getAllFilms(){
+function getAllFilms() {
     $.ajax({
-        url:"http://192.168.1.170:3000/peliculas",
-        method:"GET",
+        url: "http://192.168.1.170:3000/peliculas",
+        method: "GET",
     }).then(getSuc, getErr)
 };
-function getByID(ID){
+function getByID(ID) {
     return $.ajax({
-        method:"GET",
-        url:"http://192.168.1.170:3000/peliculas"+ID
+        method: "GET",
+        url: "http://192.168.1.170:3000/peliculas" + ID
     })
 }
 
 
-function getSuc(response){
+function getSuc(response) {
     listFilms.setMovies(response);
     listFilms.listAll();
 };
-function getErr(errorr){
-    
+function getErr(errorr) {
+
 
 };
-var listFilms={
-    data:[],
-    setMovies: function(Data){
-        this.data= Data;
+var listFilms = {
+    data: [],
+    setMovies: function (Data) {
+        console.log(Data)
+        listFilms.data = Data;
     },
-    listAll: function(){
-        for(var i = 0; i < this.data.length; i++){
-            $("#table-body").append(listFilms.assamble(this.data[i]));
+    listAll: function () {
+        for (var i = 0; i < listFilms.data.length; i++) {
+            console.log ('axel',i,listFilms.data.length)
+            listFilms.assamble(listFilms.data[i])
+            $("#table-body").append();
         }
-    
-     $("action").ON("click",function(){
-        var ID=$(this).attr("data-ID")
-        console.log(ID)
-        getByID(ID).then(function(res){
-            console.log(res);
-            $(".wrapper").html(oneassamble(res))
-      
-    $(".back").on("click",function(){
-        clear();
-        getAllFilms();
-    });
-      })
-    });
-    },    
-    
-    
-    assamble:function(Data){
-        return  '<li class="action" data-id="'+Data._id+'">' +
-        '<img src="' + Data.poster +'">'+
-        '<p>'+ Data.title +'</p>' +
-        '<p>'+ Data.gender +'</p>'+
-        '<p>'+ Data.score +'</p>'+
-        '</li>';
+
+        $("action").on("click", function () {
+            clear();
+            var ID = $(this).attr("data-ID")
+            console.log(ID)
+            getByID(ID).then(function (res) {
+                console.log(res);
+                $(".wrapper").html(oneassamble(res))
+
+                $(".back").on("click", function () {
+                    clear();
+                    getAllFilms();
+
+                });
+            })
+        });
     },
 
-    
-};
 
-getAllFilms();
+    assamble: function (Data) {
+       
+        return '<li class="action" data-id="' + Data._id + '">' +
+            '<img src="' + Data.poster + '">' +
+            '<p>' + Data.title + '</p>' +
+            '<p>' + Data.gender + '</p>' +
+            '<p>' + Data.score + '</p>' +
+            '</li>';
+    }
+
+
+};
+$(document).ready(function(){
+    getAllFilms();
+})
+
 
 
 function sendData(params) {
     return $.ajax({
         method: "POST",
-        url: "http://192.168.1.115:3000/peliculas",
+        url: "http://192.168.1.170:3000/peliculas",
         data: params,
     })
 
@@ -112,8 +120,8 @@ function clear() {
 
 function oneassamble(data) {
     return '<article>' +
-    ' <button>Back</button>'
-        +'<img class="carteleras" src="' + data.poster + '" />'
+        ' <button>Back</button>'
+        + '<img class="carteleras" src="' + data.poster + '" />'
         + '<h5>' + data.title + '</h5>'
         + '<br>'
         + '<p class="info">' + data.gender + '</p>'
